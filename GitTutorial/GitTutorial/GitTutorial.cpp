9c9e0de2317a27a4a568aa8e3b9b30ad4f5bdbcf
104820022 <104820022>
2018-03-09 11:57:42 +0800

    practice 8A-4

--- GitTutorial/GitTutorial/GitTutorial.cpp
+++ GitTutorial/GitTutorial/GitTutorial.cpp
@@ -3,6 +3,20 @@
 #include <iostream>
 using namespace System;
 bool Guess(int number) {
+	static int target = -1;
+	if (target == -1) {
+		Random r;
+		target = r.Next() % 100 + 1;
+	}
+	if (number > target) {
+		std::cout << "Smaller" << std::endl;
+		return false;
+	}
+	else if (number < target) {
+		std::cout << "Bigger" << std::endl;
+		return false;
+	}
+	return true;
 }
 int main(array<System::String ^> ^args)
 {
@@ -11,5 +25,6 @@ int main(array<System::String ^> ^args)
 		std::cout << "Choose a number between 1 - 100 :";
 		std::cin >> guess;
 	} while (!Guess(guess));
-	return 0;
+	return 0;
+
 }
